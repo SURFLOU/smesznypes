18c48dfcfbdcdbbe5a7165c4d358ecf58966a186
SURFLOU <dahike666@gmail.com>
2018-09-22 22:42:40 +0200

    ADAD

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    messageA = "Jakie s¹ godziny pracy?"
+                    messageA = "Jakie s\u0105 godziny pracy?"
                     if "text" in messaging_event["message"]:
                         message_text = messaging_event["message"]["text"]  # the message's text
                         if message_text == messageA:
